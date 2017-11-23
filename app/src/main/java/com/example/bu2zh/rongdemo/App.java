4b0414b17dccc4c385f654d7e16fd3ff95cc9ab4
罗燕龙 <luoyanlong@rongcloud.cn>
2017-11-23 09:54:16 +0800

    自定义消息

--- app/src/main/java/com/example/bu2zh/rongdemo/App.java
+++ app/src/main/java/com/example/bu2zh/rongdemo/App.java
@@ -1,8 +1,14 @@
 package com.example.bu2zh.rongdemo;
 
+import android.app.ActivityManager;
 import android.app.Application;
+import android.content.Context;
+
+import com.example.bu2zh.rongdemo.custommessage.CustomizeMessage;
 
 import io.rong.imkit.RongIM;
+import io.rong.imlib.AnnotationNotFoundException;
+import io.rong.imlib.RongIMClient;
 
 /**
  * Created by bu2zh on 2017/11/15.
@@ -13,6 +19,38 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        RongIM.init(this);
+
+        /*
+         * OnCreate 会被多个进程重入，这段保护代码，确保只有您需要使用 RongIMClient 的进程和 Push 进程执行了 init。
+         * io.rong.push 为融云 push 进程名称，不可修改。
+         */
+        if (getApplicationInfo().packageName.equals(getCurProcessName(getApplicationContext())) ||
+                "io.rong.push".equals(getCurProcessName(getApplicationContext()))) {
+            RongIM.init(this);
+        }
+
+        /*
+         * 用于自定义消息的注册, 注册后方能正确识别自定义消息, 建议在init后及时注册，保证自定义消息到达时能正确解析。
+         */
+        try {
+            RongIMClient.registerMessageType(CustomizeMessage.class);
+        } catch (AnnotationNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        AppContext.getInstance().init(getApplicationContext());
+        AppContext.getInstance().registerReceiveMessageListener();
+    }
+
+    public static String getCurProcessName(Context context) {
+        int pid = android.os.Process.myPid();
+
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+
+        for (ActivityManager.RunningAppProcessInfo appProcess : activityManager.getRunningAppProcesses()) {
+            if (appProcess.pid == pid)
+                return appProcess.processName;
+        }
+        return null;
     }
 }
---
+++ app/src/main/java/com/example/bu2zh/rongdemo/custommessage/CustomizeMessage.java
@@ -0,0 +1,97 @@
+package com.example.bu2zh.rongdemo.custommessage;
+
+
+import android.os.Parcel;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import io.rong.imlib.MessageTag;
+import io.rong.imlib.model.MessageContent;
+
+/**
+ * 自定义消息
+ */
+
+@MessageTag(value = "app:custom", flag = MessageTag.ISCOUNTED | MessageTag.ISPERSISTED)
+public class CustomizeMessage extends MessageContent {
+
+    private String mContent;
+
+    public String getContent() {
+        return mContent;
+    }
+
+    /**
+     * 发消息时调用，将自定义消息对象序列化为消息数据:
+     * 首先将消息属性封装成json，再将json转换成byte数组
+     */
+    @Override
+    public byte[] encode() {
+        JSONObject jsonObj = new JSONObject();
+
+        try {
+            jsonObj.put("content", mContent);
+        } catch (JSONException e) {
+            Log.e("JSONException", e.getMessage());
+        }
+
+        try {
+            return jsonObj.toString().getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public CustomizeMessage(byte[] data) {
+        String jsonStr = null;
+
+        try {
+            jsonStr = new String(data, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            JSONObject jsonObj = new JSONObject(jsonStr);
+
+            if (jsonObj.has("content"))
+                mContent = jsonObj.optString("content");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mContent);
+    }
+
+    protected CustomizeMessage(Parcel in) {
+        this.mContent = in.readString();
+    }
+
+    public static final Creator<CustomizeMessage> CREATOR = new Creator<CustomizeMessage>() {
+        @Override
+        public CustomizeMessage createFromParcel(Parcel source) {
+            return new CustomizeMessage(source);
+        }
+
+        @Override
+        public CustomizeMessage[] newArray(int size) {
+            return new CustomizeMessage[size];
+        }
+    };
+}
