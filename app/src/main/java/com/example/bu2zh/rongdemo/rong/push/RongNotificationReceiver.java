6130f2831138335494b29614caf0f23975666597
罗燕龙 <luoyanlong@rongcloud.cn>
2017-11-24 14:06:10 +0800

    rename

--- app/src/main/java/com/example/bu2zh/rongdemo/rong/push/RongNotificationReceiver.java
+++ app/src/main/java/com/example/bu2zh/rongdemo/rong/push/RongNotificationReceiver.java
@@ -5,7 +5,11 @@ import android.content.Context;
 import io.rong.push.notification.PushMessageReceiver;
 import io.rong.push.notification.PushNotificationMessage;
 
-public class DemoNotificationReceiver extends PushMessageReceiver {
+/**
+ * 自定义广播接收器<br>
+ * 当您的应用处于后台运行或者和融云服务器 disconnect() 的时候，如果收到消息，融云 SDK 会以通知形式提醒您
+ */
+public class RongNotificationReceiver extends PushMessageReceiver {
     @Override
     public boolean onNotificationMessageArrived(Context context, PushNotificationMessage pushNotificationMessage) {
         return false;
