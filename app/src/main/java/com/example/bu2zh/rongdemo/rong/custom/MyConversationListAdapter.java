9e905b40f1e68087e3d678cc9a2fcbd69afdb4ad
罗燕龙 <luoyanlong@rongcloud.cn>
2018-08-30 15:50:37 +0800

    修改问题

--- app/src/main/java/com/example/bu2zh/rongdemo/rong/custom/MyConversationListAdapter.java
+++ app/src/main/java/com/example/bu2zh/rongdemo/rong/custom/MyConversationListAdapter.java
@@ -3,8 +3,6 @@ package com.example.bu2zh.rongdemo.rong.custom;
 import android.content.Context;
 import android.view.View;
 
-import io.rong.imkit.RongContext;
-import io.rong.imkit.model.ConversationKey;
 import io.rong.imkit.model.UIConversation;
 import io.rong.imkit.widget.adapter.ConversationListAdapter;
 import io.rong.imlib.model.Conversation;
@@ -14,19 +12,18 @@ import io.rong.imlib.model.Conversation;
  */
 
 public class ConversationListAdapterEx extends ConversationListAdapter {
+
     public ConversationListAdapterEx(Context context) {
         super(context);
     }
 
     @Override
     protected void bindView(View v, int position, UIConversation data) {
-        if (data != null) {
-            ConversationKey key = ConversationKey.obtain(data.getConversationTargetId(), data.getConversationType());
-            Conversation.ConversationNotificationStatus status = RongContext.getInstance().getConversationNotifyStatusFromCache(key);
-            if (status == Conversation.ConversationNotificationStatus.DO_NOT_DISTURB) {
-                data.setUnreadType(UIConversation.UnreadRemindType.REMIND_ONLY);
-            }
-        }
         super.bindView(v, position, data);
+        if (data != null) {
+            ViewHolder holder = (ViewHolder) v.getTag();
+            if (data.getConversationGatherState() && data.getConversationType() == Conversation.ConversationType.SYSTEM)
+            holder.leftImageView.setAvatar();
+        }
     }
 }
---
+++ app/src/test/java/com/example/bu2zh/rongdemo/HanziToPinyinTest.java
@@ -0,0 +1,7 @@
+package com.example.bu2zh.rongdemo;
+
+/**
+ * Created by luoyanlong on 2018/07/10.
+ */
+public class HanziToPinyinTest {
+}
